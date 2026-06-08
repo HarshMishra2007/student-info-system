2676ed19274d66bf720728db64502bab45207385
Harsh Mishra <85541359+HarshMishra2007@users.noreply.github.com>
2026-06-08 12:54:55 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# Student Info System
+
+name = "Harsh Mishra"
+age = 17
+college_year = "First Year (Upcoming)"
+
+print("=== STUDENT INFO SYSTEM ===")
+print("Name:", name)
+print("Age:", age)
+print("College Year:", college_year)
+
+marks = 85
+
+if marks > 80:
+    print("Performance: Excellent")
+elif marks > 60:
+    print("Performance: Good")
+else:
+    print("Performance: Needs Improvement")
